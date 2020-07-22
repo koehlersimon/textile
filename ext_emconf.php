df91e36fdf4f57f6c5e340327b8017768453f115
Simon Köhler <info@simon-koehler.com>
2020-07-22 06:32:23 -0500

    update

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,11 +1,11 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Textile Markdown Parser',
-    'description' => 'Write Markdown code in the TYPO3 backend',
+    'title' => 'Textile Markdown',
+    'description' => 'Provides a new Markdown content element and a ViewHelper',
     'category' => 'plugin',
     'author' => 'Simon Köhler',
     'author_email' => 'info@simon-koehler.com',
-    'state' => 'alpha',
+    'state' => 'beta',
     'clearCacheOnLoad' => 0,
     'version' => '1.0.0',
     'constraints' => [
