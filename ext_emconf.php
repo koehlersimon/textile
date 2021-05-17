292fad6f45c557366b2424a2f202dd60846f7875
koehlersimon <info@simon-koehler.com>
2021-05-17 14:25:38 -0500

    Updates

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,12 +5,13 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'plugin',
     'author' => 'Simon Köhler',
     'author_email' => 'info@simon-koehler.com',
+    'company' => 'simon-koehler.com',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.99-10.4.99',
+            'typo3' => '9.5.99-11.2.99',
         ],
         'conflicts' => [],
         'suggests' => [],
